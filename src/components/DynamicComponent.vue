1f1afc0ffccfd80c10e6beab16163b4b96c4d6d9
ryudain05 <dkdlsekdls@naver.com>
2025-03-20 10:12:42 +0900

    Feat: Dynamic Components

---
+++ src/components/DynamicComponent.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="container py-4"></div>
+	<button class="btn btn-primary me-2" @click="changeCurrentComp(DynamicApple)">
+		사과
+	</button>
+	<button class="btn btn-danger" @click="changeCurrentComp(DynamicBanana)">
+		바나나
+	</button>
+	<hr />
+	<component :is="currentComp"></component>
+</template>
+
+<script setup>
+import DynamicApple from './DynamicApple.vue';
+import DynamicBanana from './DynamicBanana.vue';
+import { shallowRef } from 'vue';
+
+const currentComp = shallowRef(DynamicApple);
+const changeCurrentComp = comp => (currentComp.value = comp);
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -1,10 +1,12 @@
 <template>
 	<main>
 		<ScriptSetup />
+		<DynamicComponent />
 	</main>
 </template>
 
 <script setup>
+import DynamicComponent from './DynamicComponent.vue';
 import ScriptSetup from './ScriptSetup.vue';
 </script>
 
