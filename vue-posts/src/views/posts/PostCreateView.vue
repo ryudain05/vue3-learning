a17fef3265e88805bfcf8dbcb5dcaf708caca2a2
ryudain05 <dkdlsekdls@naver.com>
2025-03-20 16:00:53 +0900

    Design: 게시판 UI 전체 Add

---
+++ vue-posts/src/views/posts/PostCreateView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>게시글 등록</h2>
+    <hr class="my-4" />
+    <form @submit.prevent>
+      <div class="mb-3">
+        <label for="title" class="form-label">제목</label>
+        <input
+          type="text"
+          class="form-control"
+          id="title"
+          placeholder="제목을 입력해주세요."
+        />
+      </div>
+      <div class="mb-3">
+        <label for="content" class="form-label">내용</label>
+        <textarea
+          class="form-control"
+          id="content"
+          placeholder="내용을 입력해주세요."
+          rows="8"
+        ></textarea>
+      </div>
+      <div class="pt-4">
+        <button
+          type="button"
+          class="btn btn-outline-dark me-2"
+          @click="goListPage"
+        >
+          목록
+        </button>
+        <button type="button" class="btn btn-dark">저장</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const goListPage = () => {
+  router.push({ name: 'PostList' });
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue-posts/src/views/posts/PostEditView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>게시글 수정</h2>
+    <hr class="my-4" />
+    <form @submit.prevent>
+      <div class="mb-3">
+        <label for="title" class="form-label">제목</label>
+        <input
+          type="text"
+          class="form-control"
+          id="title"
+          placeholder="제목을 입력해주세요."
+        />
+      </div>
+      <div class="mb-3">
+        <label for="content" class="form-label">내용</label>
+        <textarea
+          class="form-control"
+          id="content"
+          placeholder="내용을 입력해주세요."
+          rows="8"
+        ></textarea>
+      </div>
+      <div class="pt-4">
+        <button
+          type="button"
+          class="btn btn-outline-danger me-2"
+          @click="goDetailPage"
+        >
+          취소
+        </button>
+        <button type="button" class="btn btn-dark">수정</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+const id = route.params.id;
+
+const goDetailPage = () => router.push({ name: 'PostDetail', params: { id } });
+</script>
+
+<style lang="scss" scoped></style>
